bed3344355005607761fdce9ef37a33d523e6c47
igor20192 <igor.udovenko2012@meta.ua>
2023-12-04 17:53:20 +0200

    urls.py add path static

--- shopping_online_tour/urls.py
+++ shopping_online_tour/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.apps import apps
+from django.conf import settings
+from django.conf.urls.static import static
 
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("i18n/", include("django.conf.urls.i18n")),
-    path("", include(apps.get_app_config("oscar").urls[0])),
-]
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("i18n/", include("django.conf.urls.i18n")),
+        path("", include(apps.get_app_config("oscar").urls[0])),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
