04907fbcd55e24d2b66ccba40ff0164568ced293
igor20192 <igor.udovenko2012@meta.ua>
2023-12-12 12:56:36 +0200

    checkout.apps.py add api_nova_poshta_warehouses/,api_nova_poshta_cities/

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,7 +1,32 @@
 from django.apps import AppConfig
-from oscar.apps.checkout.apps import *
+from oscar.apps.checkout.apps import CheckoutConfig as CoreCheckoutConfig
+from .views import NovaPoshtaWarehousesAPIView, NovaPoshtaCityAPIView
+from django.urls import path
 
 
-class CheckoutConfig(CheckoutConfig):
+class CheckoutConfig(CoreCheckoutConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "checkout"
+
+    def ready(self):
+        super().ready()
+        self.extra_view = NovaPoshtaWarehousesAPIView
+
+    def get_urls(self):
+        urls = super().get_urls()
+        urls += [
+            path(
+                "api_nova_poshta_warehouses/<str:city>/",
+                self.extra_view.as_view(),
+                name="nova_poshta_warehouses",
+            ),
+        ]
+        urls += [
+            path(
+                "api_nova_poshta_cities/",
+                NovaPoshtaCityAPIView.as_view(),
+                name="api_nova_poshta_cities",
+            )
+        ]
+
+        return self.post_process_urls(urls)
