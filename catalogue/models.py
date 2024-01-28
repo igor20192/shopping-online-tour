9a9f4de8b4f97d7e8921a2de9c95e324750b3491
igor20192 <igor.udovenko2012@meta.ua>
2024-01-28 23:15:13 +0200

    catalogue models.py add keywords

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,1 +1,14 @@
+from django.db import models
+from oscar.apps.catalogue.abstract_models import AbstractCategory, AbstractProduct
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(AbstractCategory):
+    keywords = models.TextField(_("keywords"), blank=True)
+
+
+class Product(AbstractProduct):
+    keywords = models.TextField(_("keywords"), blank=True)
+
+
 from oscar.apps.catalogue.models import *
